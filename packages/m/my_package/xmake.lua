39f0284a585ec125af349a839a9121f86d57cb8c
Miouzora <alexandre.franquet@epitech.eu>
2024-11-07 22:23:37 +0900

    Update xmake.lua

--- packages/m/my_package/xmake.lua
+++ packages/m/my_package/xmake.lua
@@ -4,7 +4,6 @@ package("my_package")
     set_license("MIT")
 
     add_urls("https://github.com/Miou-zora/HelloWorldXmakePackage-Lib.git")
-    add_versions("v1.0", "aab3a970701a263ce75a172b498f332d6a56056d")
 
     on_install(function (package)
         local configs = {}
